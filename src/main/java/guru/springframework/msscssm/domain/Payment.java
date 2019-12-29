1b28f480b35a3304f0f69e471f2fb2c1b823f06c
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2019-12-29 14:50:57 -0500

    CircleCI config update

--- src/main/java/guru/springframework/msscssm/domain/Payment.java
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 public class Payment
 {
     @Id
+    @GeneratedValue
     private Long id;
 
     @Enumerated(EnumType.STRING)
