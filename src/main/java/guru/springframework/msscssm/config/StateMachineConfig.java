3f49063ec1d142e9cafa1944bf19dcd3a32ce909
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2019-12-29 14:53:27 -0500

    CircleCI config update

---
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent>
+{
+}
