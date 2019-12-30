d2a065c67003d29c21d696201bc3a14c9d37db28
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2019-12-29 21:25:16 -0500

    Completed lecture 209

--- src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -6,6 +6,8 @@ import guru.springframework.msscssm.domain.PaymentState;
 import guru.springframework.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
@@ -16,8 +18,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService
 {
+    public static final String PAYMENT_ID_HEADER = "payment_id";
+
     private final PaymentRepository paymentRepository;
     private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+    private final PaymentStateChangeInterceptor paymentStateChangeInterceptor;
 
     @Override
     public Payment newPayment(Payment payment)
@@ -30,6 +35,7 @@ public class PaymentServiceImpl implements PaymentService
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId)
     {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORIZE);
         return null;
     }
 
@@ -37,6 +43,7 @@ public class PaymentServiceImpl implements PaymentService
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId)
     {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_APPROVED);
         return null;
     }
 
@@ -44,7 +51,18 @@ public class PaymentServiceImpl implements PaymentService
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId)
     {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_DECLINED);
         return null;
+    }
+
+    private void sendEvent(Long paymentId,
+                           StateMachine<PaymentState, PaymentEvent> sm,
+                           PaymentEvent paymentEvent)
+    {
+        Message msg = MessageBuilder.withPayload(paymentEvent)
+                                    .setHeader(PAYMENT_ID_HEADER, paymentId)
+                                    .build();
+        sm.sendEvent(msg);
     }
 
     private StateMachine<PaymentState, PaymentEvent> build(Long paymentId)
@@ -57,10 +75,11 @@ public class PaymentServiceImpl implements PaymentService
         sm.getStateMachineAccessor()
           .doWithAllRegions(sma ->
                             {
-                                sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getPaymentState(),
-                                                                                       null,
-                                                                                       null,
-                                                                                       null));
+                                sma.addStateMachineInterceptor(paymentStateChangeInterceptor);
+                                sma.resetStateMachine(
+                                        new DefaultStateMachineContext<>(payment.getPaymentState(),
+                                                                         null,
+                                                                         null, null));
                             });
         sm.start();
         return sm;
